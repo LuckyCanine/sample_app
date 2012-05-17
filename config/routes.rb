e5232f35dbb30a062a1155ea19c1fb3c23f5903f
LuckyCanine <tarun242@gmail.com>
2012-05-17 13:08:34 -0700

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
  
+  resources :users
   match '/signup', to: 'users#new'
 
   root to: 'static_pages#home'  
@@ -7,7 +8,7 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
